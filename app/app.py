513a2dee93f94a4af6c0dec3c1fbcae960807381
BIOPZ-Kanitz Alexander <alexander.kanitz@unibas.ch>
2018-08-21 15:44:25 +0200

    Aborting if config environment variable is unset or file is not found.

--- app/app.py
+++ app/app.py
@@ -2,20 +2,30 @@ from configloader import ConfigLoader
 from connexion import App
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+from pathlib import Path
+import os, sys
 
 
 # Parse config file
 config = ConfigLoader()
+if 'WES_CONFIG' not in os.environ:
+    sys.exit("Environment variable 'WES_CONFIG' not set.\nExecution aborted.")
+if not os.path.isfile(os.environ['WES_CONFIG']):
+    sys.exit("No config file found at location specified in 'WES_CONFIG': {}.\nExecution aborted.".format(os.environ['WES_CONFIG']))
 config.update_from_yaml_env('WES_CONFIG')
 
 
 # Initialize app
-app = App(
-    __name__,
-    specification_dir=config['openapi']['path'],
-    swagger_ui=True,
-    swagger_json=True
-)
+try:
+    app = App(
+        __name__,
+        specification_dir=config['openapi']['path'],
+        swagger_ui=True,
+        swagger_json=True
+    )
+except KeyError:
+    sys.exit("Config file corrupt. Execution aborted.")
+
 app.app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app.app)
 
@@ -37,9 +47,12 @@ def configure_app(app):
 
 def add_settings(app):
     '''Add settings to Flask app instance'''
-    app.host = config['server']['host']
-    app.port = config['server']['port']
-    app.debug = config['server']['debug']
+    try:
+        app.host = config['server']['host']
+        app.port = config['server']['port']
+        app.debug = config['server']['debug']
+    except KeyError:
+        sys.exit("Config file corrupt. Execution aborted.")
 
     return(app)
 
@@ -53,10 +66,13 @@ def apply_cors(app):
 
 def add_openapi(app):
     '''Add OpenAPI specification to connexion app instance'''
-    app.add_api(
-        config['openapi']['yaml_specs'],
-        validate_responses=True,  # FIXME: This does not seem to work
-    )
+    try:
+        app.add_api(
+            config['openapi']['yaml_specs'],
+            validate_responses=True,  # FIXME: This does not seem to work
+        )
+    except KeyError:
+        sys.exit("Config file corrupt. Execution aborted.")
 
     return(app)
 
