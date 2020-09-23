e7fdd714c5534c57856679d693ffbd9b4701e658
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-09-23 08:48:31 +0200

    bump supported Python versions

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=['connexion', 'Flask-Cors', 'Flask-PyMongo'],
 )
